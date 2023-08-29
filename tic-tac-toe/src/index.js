d6fb91c116ff338981d2e4635c13024d062f2952
Vincent Chen <asd1111zxcv@gmail.com>
2023-08-29 22:20:17 +0800

    0829update

--- tic-tac-toe/src/index.js
+++ tic-tac-toe/src/index.js
@@ -36,16 +36,46 @@ class Board extends React.Component{
     return <div className='Board'>{cells}</div>
   }
   Update(index){
-    this.setState((currentState)=>{
+    this.setState((preState)=> {
+      let currentMark = preState.marks;
+      if (preState.winner===null && currentMark[index]===-1){
+        currentMark[index]=preState.circle%2;
+      }
+      // let winner = this.CheckWinner(preState.currentMark);
+      return {
+        circle:preState.circle+1,
+        marks:currentMark,
+        winner:null
+      }
     })
+  }
+  CheckWinner(mark){
+    for (let y=0;y<3;y++){
+      if (mark[3*y]!==-1 && mark[3*y]===mark[3*y+1] && mark[3*y]===mark[3*y+2]){
+        return {side: mark[3*y], startIndex: 3*y, endIndex: 3*y+2}
+      }
+    }
+    for (let x=0;x<3;x++){
+      if (mark[x]!==-1 && mark[x]===mark[3+x] && mark[x]===mark[x+6]){
+        return {side: mark[x], startIndex: x, endIndex: x+6}
+      }
+    }
+    if(mark[0]!==-1&&mark[0]===mark[4]&&mark[4]===mark[8]){
+      return {side:mark[0], startIndex:0, endIndex:8};
+    }else if(mark[2]!==-1&&mark[2]===mark[4]&&mark[4]===mark[6]){
+      return {side:mark[2], startIndex:2, endIndex:6};
+    }
+    // 目前還沒有贏家
+    return null;
+
+
   }
 }
 
 
 
 
-
-
-
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<Board/>);
+
+
